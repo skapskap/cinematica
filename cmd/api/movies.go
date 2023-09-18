e79eee52b70e4746b047daeb41642c7e22499de5
skapskap <skaprange@gmail.com>
2023-09-18 15:02:36 -0300

    esqueleto inicial do projeto

---
+++ cmd/api/movies.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := chi.URLParam(r, "id")
+
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of the movie %d\n", id)
+}
