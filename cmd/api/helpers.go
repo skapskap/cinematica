b42c631ca9c04dda56c8e66c1f08effff94f7ff4
skapskap <skaprange@gmail.com>
2023-09-19 12:48:48 -0300

    movie struct e tratamento dos json

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+type envelope map[string]interface{}
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	idStr := chi.URLParam(r, "id")
 
@@ -19,9 +21,9 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{},
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope,
 	headers http.Header) error {
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/skapskap/cinematica/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +17,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "O servidor encontrou um problema e não processou sua requisição", http.StatusInternalServerError)
+	}
 }
